55d5cd8bcabde7d0be491ba7b8c89606786bf7a5
MoheyElDin Badr <MoheyElDin.Badr@gmail.com>
2023-11-13 23:51:34 +0200

    Initial commit

---
+++ src/main/java/com/example/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class UserDTO {
+    private final String id;
+    private final String accessToken;
+
+    public UserDTO(String id, String accessToken) {
+        this.id = id;
+        this.accessToken = accessToken;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+}
